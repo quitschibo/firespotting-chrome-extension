975f0c6b2f8acf8be3709ba22c37edd0ebf010f2
Manuel Möhlmann <mail@manmoe.com>
2013-09-05 17:29:16 -0400

    move crx file to correct folder

--- crxmake.sh
+++ crxmake.sh
@@ -51,4 +51,4 @@ sig_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$sig" | awk '{print $5}')))
 ) > "$crx"
 echo "Wrote $crx"
 rm $key
-mv $crx src/main/resources
+mv $crx selenium-tests/src/main/resources
