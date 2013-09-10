bdfa9e72924af0834087238c58d03b900a240e28
Manuel Möhlmann <mail@manmoe.com>
2013-09-10 15:44:09 -0400

    notifications are active per default

--- src/background.js
+++ src/background.js
@@ -6,9 +6,10 @@ function startRequest() {
 }
 
 if (localStorage["FS.Notifications"] == null) {
+	// TODO: html notifications don't work anymore. port it to simple notifications or delete it
 	var notification = webkitNotifications.createHTMLNotification("initialNotification.html");
 	notification.show();
-	localStorage["FS.Notifications"] = false;
+	localStorage["FS.Notifications"] = true;
 }
 //If any options are not already set, they will be set to defaults here
 SetInitialOption("FS.RequestInterval", 1200000);
