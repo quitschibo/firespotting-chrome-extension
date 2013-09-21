a5e70cb2c57d5d74b668ce4d20e686323b4228ab
Manuel Möhlmann <mail@manmoe.com>
2013-09-21 05:58:03 -0400

    removed unused mock librarys

--- test/coreTest.js
+++ test/coreTest.js
@@ -1,9 +1,5 @@
 var assert = require("assert");
 var core = require("../src/core.js");
-var sinon = require("sinon");
-var JsMockito = require('jsmockito').JsMockito;
-JsMockito.Integration.Nodeunit();
-JsMockito.Integration.importTo( exports );
 
 describe('core.js testsuite', function() {
 	describe('#setInitialOption', function() {
